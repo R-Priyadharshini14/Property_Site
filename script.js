30158aaef2c46ea3f22e1c35c92b1752f04e9f01
Priyadharshini R <163970352+R-Priyadharshini14@users.noreply.github.com>
2024-03-19 15:29:00 +0530

    All are Updated

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buy = document.getElementById('buy');
+
+    if (buy) {
+        buy.addEventListener('click', function() {
+            window.location.href = "buy.html";
+        });
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+const rent= document.getElementById('rent');
+
+if (rent) {
+    rent.addEventListener('click', function() {
+        window.location.href = "rent.html";
+    });
+}});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const HouseLoan= document.getElementById('HouseLoan');
+    
+    if (HouseLoan) {
+        HouseLoan.addEventListener('click', function() {
+            window.location.href = "https://portal.hdfc.com/?ref_code=SEM_ApplyOnline_Desk&utm_medium=Desktop&utm_campaign=SEM&utm_content=D_Generic_Home/House/Plot_Loan+Exact_Desktop_Repli&s_kwcid=AL!13313!3!692716480746!e!!g!!homeloan&gad_source=1&gclid=Cj0KCQjwwMqvBhCtARIsAIXsZpaBNLWpXlYF2WTwxUwQUk-ItLlyd3kLiK-1DKRWVA-WMqn7CN6VJ-AaAsJiEALw_wcB";
+        });
+    }});
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        const Interiordesign= document.getElementById('Interiordesign');
+        
+        if (Interiordesign) {
+            Interiordesign.addEventListener('click', function() {
+                window.location.href = "interiordesign.html";
+
+            });
+        }});
+        
+
+        
+    
+
